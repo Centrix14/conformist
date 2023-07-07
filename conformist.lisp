895db17fc0ebc259bbe56f467305946043ed8435
Centrix14 <artiom.zvyagin@yandex.ru>
2023-07-07 10:34:30 +0300

    Lisp code from literal sources.

--- conformist.lisp
+++ conformist.lisp
@@ -3,17 +3,33 @@
 
 (in-package :conformist)
 
-(defvar *placeholders* (list :symbol :list))
+(defvar *placeholders* (make-hash-table :test #'equal))
+
+(defun define-placeholder (placeholder recognition-predicate)
+  (if (gethash placeholder *placeholders*)
+      (format t "You try to redefine an existing placeholder~%")
+      (setf (gethash placeholder *placeholders*) recognition-predicate)))
+
+(defun remove-placeholder (placeholder)
+  (if (gethash placeholder *placeholders*)
+      (remhash placeholder *placeholders*)
+      (format t "You try to remove unexisting placeholder~%")))
+
+(defun redefine-placeholder (placeholder recognition-predicate)
+  (if (gethash placeholder *placeholders*)
+      (setf (gethash placeholder *placeholders*) recognition-predicate)
+      (format t "You try to redefine unexisting placeholder~%")))
+
+(defun get-placeholder-predicate (placeholder)
+  (values (gethash placeholder *placeholders*) placeholder))
 
 (defun placeholderp (data)
-  (member data *placeholders*))
+  (if (gethash data *placeholders*)
+      t
+      nil))
 
 (defun does-placeholder-matches-data (placeholder data)
-  (cond
-    ((eql placeholder :symbol)
-     (symbolp data))
-    ((eql placeholder :list)
-     (listp data))))
+  (funcall (get-placeholder-predicate placeholder) data))
 
 (in-package :conformist)
 
