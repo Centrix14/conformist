ac939f7947ba050be85dd5ef19056e38e2641735
Centrix14 <artiom.zvyagin@yandex.ru>
2023-08-18 16:42:06 +0300

    Added all util functions.

--- src/util.lisp
+++ src/util.lisp
@@ -1,5 +1,30 @@
+(in-package :conformist)
+
 (defmacro create-hash-table (make-args &rest values)
   `(let ((tmp (make-hash-table ,@make-args)))
      (loop for pair in ',values do
        (setf (gethash (first pair) tmp) (second pair)))
      tmp))
+
+(defun frontier (sequence)
+  (1- (length sequence)))
+
+(defun elm (sequence index)
+  (if (> index (frontier sequence))
+      nil
+      (elt sequence index)))
+
+(defun listify (value)
+  (if (listp value)
+      value
+      (list value)))
+
+(defun unite (sequence)
+  (reduce (lambda (x y)
+            (append (listify x)
+                    (listify y)))
+          sequence))
+
+(defun simplep (placeholder pattern-system)
+  (with-slots (placeholders-collection) pattern-system
+      (second (gethash placeholder placeholders-collection))))
