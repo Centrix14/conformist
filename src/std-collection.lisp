39fdd1313ff5f0d059c33c6e79bbb9cc52987eb0
Centrix14 <artiom.zvyagin@yandex.ru>
2023-08-18 13:57:23 +0300

    std-collection definitions added.

--- src/std-collection.lisp
+++ src/std-collection.lisp
@@ -3,3 +3,26 @@
                    (:list (listp t))
                    (:symbols (symbolp nil))
                    (:lists (listp nil)))
+
+(defun placeholderp (data pattern-system)
+  (with-slots ((collection placeholders-collection)) pattern-system
+    (multiple-value-bind (value exists) (gethash data collection)
+      (declare (ignore value))
+      exists)))
+
+(defun does-placeholder-matches-data (placeholder data pattern-system)
+  (with-slots ((collection placeholders-collection)) pattern-system
+    (funcall (first (gethash placeholder collection)) data)))
+
+(defun does-a-matches-b (a b pattern-system)
+  (with-slots (placeholder-predicate data-to-placeholder-matcher) pattern-system
+      (if (funcall placeholder-predicate a pattern-system)
+          (funcall data-to-placeholder-matcher a b pattern-system)
+          (equalp a b))))
+
+(defun match-complex-placeholder (data index edge-placeholder pattern-system)
+  (with-slots ((matcher single-values-matcher)) pattern-system
+    (loop for i from index to (frontier data)
+          while (not
+                 (funcall matcher edge-placeholder (elt data i) pattern-system))
+          collect (elt data i))))
